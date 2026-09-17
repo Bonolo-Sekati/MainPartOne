cac034871306c1350cdff1f8345e4ffdeca705ee
Student <Student@LabVM2049939.2dazmvau2gle3k2gjfrpdjjipa.jnbx.internal.cloudapp.net>
2026-09-17 11:30:36 +0200

    Initial commit

--- src/main/java/com/mycompany/mainpartone/LoginPartOne.java
+++ src/main/java/com/mycompany/mainpartone/LoginPartOne.java
@@ -10,6 +10,7 @@ import java.util.regex.Pattern;
  */
 /**
  * Oracle, 2025. String (Java Platform SE). Available at: https://oracle-java-string-documentation/ [Accessed 16 September 2026].
+ * Farrell, 2018. Programming Logic and Design. 9th edn. Boston, MA: Cengage Learning.
  */
 
 public class LoginPartOne {
